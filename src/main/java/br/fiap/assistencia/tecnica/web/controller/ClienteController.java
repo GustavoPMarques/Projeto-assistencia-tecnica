2c11a8ccb88322b599f0e4b6503aa5c0183dd99e
Laboratório FIAP <labsfiap@fiap.com.br>
2025-10-16 11:43:50 -0300

    alteracao controller

--- src/main/java/br/fiap/assistencia/tecnica/web/controller/ClienteController.java
+++ src/main/java/br/fiap/assistencia/tecnica/web/controller/ClienteController.java
@@ -1,4 +1,23 @@
 package br.fiap.assistencia.tecnica.web.controller;
 
+import br.fiap.assistencia.tecnica.service.ClienteService;
+import br.fiap.assistencia.tecnica.web.dto.ClienteDTO;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/cliente")
 public class ClienteController {
+    private final ClienteService service;
+
+    public ClienteController(ClienteService service) {
+        this.service = service;
+    }
+
+    @PostMapping
+    public void cadastrar(@RequestBody ClienteDTO clienteDTO) {
+
+    }
 }
